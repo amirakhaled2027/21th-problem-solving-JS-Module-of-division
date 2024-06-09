07ceb20dadf3b2a2d0def1ff39bfe2274b20631f
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-06-09 22:30:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//create a function that return the module of the division and not the integer
+function divisionRemainder(a , b) {
+    var divide = a % b;
+    return divide;
+  }
+  console.log(divisionRemainder(13 , 2));
